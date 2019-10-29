0530f4633a4fefb894a0926cf9aa2f297f51cb78
svend <sven.depickere@student.howest.be>
2019-10-29 11:54:26 +0100

    Update rna-transcription.ts

--- Typescript/typescript/rna-transcription/rna-transcription.ts
+++ Typescript/typescript/rna-transcription/rna-transcription.ts
@@ -1,6 +1,32 @@
 class Transcriptor {
-    toRna( /* Parameters go here */ ) {
-        // Your code here
+    private DNAStrandArr: string[] | undefined;
+    private RNATrans: {
+        "G": {
+            name: "C"
+        },
+        "C": {
+            name: "G"
+        },
+        "T": {
+            name: "A"
+        },
+        "A": {
+            name: "U"
+        },
+    } | undefined;
+
+    toRna(DNAStrand: string) : string {
+        this.DNAStrandArr = [...DNAStrand];
+        if (this.DNAStrandArr.length > 1) {
+            return this.translateAllElements(this.DNAStrandArr).toString()
+        } else {
+            return this.RNATrans[this.DNAStrandArr[0]].name
+        }
+    }
+
+    translateAllElements(DNAStrandArr: string[]) : string[] {
+        this.DNAStrandArr = DNAStrandArr;
+        return ['test'];
     }
 }
 
